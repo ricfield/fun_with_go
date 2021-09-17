d389d96a8cad4103804f0272fbea99d65c557066
ricfield <ricfield@gmail.com>
2021-09-16 21:42:54 -0400

    Update food.go

--- food.go
+++ food.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("What would you like to eat?")
-  
   var food string
+  
+  fmt.Println("What would you like to eat?")
   fmt.Scan(&food)
   
   fmt.Printf("Sure, we can have %v.\n", food)
