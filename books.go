49221afe0e05014c650a025b3ff0ac45e62208a5
ricfield <ricfield@gmail.com>
2021-09-16 20:51:20 -0400

    Create books.go

---
+++ books.go
@@ -0,0 +1,45 @@
+package main
+import "fmt"
+
+
+func main() {
+   var publisher string
+   var writer string
+   var artist string
+   var title string
+   var year int
+   var pageNumber int
+   var grade float32
+
+   title = "Mr.GoToSleep"
+   writer = "Tracey Hatchet"
+   artist = "Jewel Tampson"
+   publisher = "DizzyBooks Publishing Inc."
+   year = 1997
+   pageNumber = 14
+   grade = 6.5
+
+
+   fmt.Println("Publisher: " + publisher)
+   fmt.Println("Writer: " + writer)
+   fmt.Println("Artist: " + artist)
+   fmt.Println("Title: " + title)
+   fmt.Println("Year:", year)
+   fmt.Println("Total Pages:", pageNumber)
+   fmt.Println("Grade:", grade)
+
+   writer = "Ryan N. Shawn"
+   artist = "Phoebe Paperclips"
+   year = 2013
+   pageNumber = 160
+   grade = 9.0
+
+   fmt.Println("\nPublisher: " + publisher)
+   fmt.Println("Writer: " + writer)
+   fmt.Println("Artist: " + artist)
+   fmt.Println("Title: " + title)
+   fmt.Println("Year:", year)
+   fmt.Println("Total Pages:", pageNumber)
+   fmt.Println("Grade:", grade)
+
+}
