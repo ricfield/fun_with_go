7ef6416f047dce0b102b0d6052336f8cce8ac076
ricfield <ricfield@gmail.com>
2021-09-17 02:35:27 -0400

    Add files via upload

---
+++ btc_json.go
@@ -0,0 +1,35 @@
+package main
+
+import ("net/http"; "fmt"; "io/ioutil"; "time"; "encoding/json")
+
+type api_data struct {
+   Base string
+   Currency string
+   Amount string
+}
+   
+type data struct {
+   Data api_data
+}
+
+func main() {
+   var j data
+   
+   fmt.Printf("Time                 Base   Currency    Amount\n")
+   fmt.Printf("------------------------------------------------\n")
+   
+   for i := 1; i <= 100; i++ {
+   
+      resp, err := http.Get("https://api.coinbase.com/v2/prices/spot?currency=USD")
+      contents, err := ioutil.ReadAll(resp.Body)
+      json.Unmarshal(contents, &j)
+	  
+      if err != nil {
+      }
+	
+	
+	  fmt.Printf("%s  %s    %s         %s     \r", time.Now().Format("01-2-2006 15:04:05"), j.Data.Base, j.Data.Currency, j.Data.Amount)
+
+	  time.Sleep(time.Second * 5)
+   }
+}
